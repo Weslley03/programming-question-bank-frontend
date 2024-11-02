ece5e48e53aaf871993207a341901e1029a7b77b
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-02 17:01:24 -0300

    added 'react-router', Home page and  Acesso page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { 
+  createBrowserRouter,
+  RouterProvider, 
+} from 'react-router-dom';
+import { HomePage } from './pages/Home/Home';
+import { AcessoPage } from './pages/Acesso/Acesso';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+
+  {
+    path: '/acesso',
+    element: <AcessoPage />
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
