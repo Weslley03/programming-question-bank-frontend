817fec432fdba02587fb805400885ccd17cca633
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-04 11:42:41 -0300

    done 'aceso' screen

--- src/components/CardAcesso/CardAcesso.jsx
+++ src/components/CardAcesso/CardAcesso.jsx
@@ -1,38 +1,85 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AcessoSchema } from "../../schema/acessoSchema";
+import { CadastroSchema } from "../../schema/cadastroSchema";
+import { useNavigate } from "react-router-dom";
+import { acessoUser } from "../../services/AcessoService/acessoService";
+
 export function CardAcesso( { accesOption }) {
+  let schema;
+  if(accesOption === 'register') {
+    schema = CadastroSchema;
+  } else {
+    schema = AcessoSchema;
+  };
+
+  const {
+    register: acessoRegister,
+    handleSubmit: acessoHandleSubmit,
+    formState: { errors: acessoErrors },
+  } = useForm({ resolver: zodResolver(schema) }); 
+
+  const nami = useNavigate();
+
+  async function loginHandle(data){
+    try{
+      await acessoUser(data, accesOption);
+      console.log(data)
+      if(accesOption === 'register') nami('/acesso');
+      if(accesOption === 'login') nami('/');
+    } catch(err) {
+      console.error(err);
+    };
+  }
+
   return(
     <section className="container text-center">
       <h1>Weslley-Dev</h1>
 
-      <div className="row">
-        {  
-          accesOption == 'register'
-          ? <input type="text" placeholder="username" />
-          : null 
-        }
-          <input type="text" placeholder="email" />
-          <input type="password" placeholder="password" />
+      <form onSubmit={acessoHandleSubmit(loginHandle)}>
+        <div className="row">
+          {  
+            accesOption == 'register' &&
+            <>
+              <input type="text" placeholder="name" {...acessoRegister('User_Name')} />
+              {acessoErrors.User_Name && <span> { acessoErrors.User_Name.message } </span>}
 
-        {  
-          accesOption == 'register'
-          ? <input type="text" placeholder="confirmpassword" />
-          : null 
-        }
-      </div>
+              <input type="text" placeholder="username" {...acessoRegister('User_Username')} />
+              {acessoErrors.User_Username && <span> { acessoErrors.User_Username.message } </span>}
+            </>
+          }
+            <input type="email" placeholder="email" {...acessoRegister('User_Email')} />
+            {acessoErrors.User_Email && <span> { acessoErrors.User_Email.message } </span>}
 
-      <div className="row">
-        {
-          accesOption == 'register'
-          ? null
-          : <button>entrar</button>
-        }
-        <button>cadastrar</button>
-        {
-          accesOption == 'register'
-          ? null
-          : <p>esqueci minha senha</p>
-        }
-        
-      </div>
+            <input type="password" placeholder="password"  {...acessoRegister('User_Password')} />
+            {acessoErrors.User_Password && <span> { acessoErrors.User_Password.message } </span>}
+
+          {  
+            accesOption == 'register' &&
+              <>
+                <input type="password" placeholder="confirmpassword"  {...acessoRegister('confirmpasswordData')}/>
+                {acessoErrors.confirmpasswordData && <span> { acessoErrors.confirmpasswordData.message } </span>}
+              </> 
+          }
+        </div>
+
+        <div className="row">
+          
+          {
+            accesOption == 'register'
+            ? <button type="submit">cadastrar</button>
+            : <>
+              <button type="submit">entrar</button> 
+              <button onClick={() => nami('/cadastro')} type="button">cadastro</button>
+              </> 
+          }
+          
+          {
+            accesOption !== 'register' && <p style={{cursor: "pointer"}}>esqueci minha senha</p>
+          }
+
+        </div>
+      </form>
     </section>
   );
 };
