126f5ea5da35d67574857ce28bf67c48443ee591
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-07 16:26:52 -0300

    deleted the `console.log`

--- src/services/ResetPassword/resetPasswordService.js
+++ src/services/ResetPassword/resetPasswordService.js
@@ -5,10 +5,8 @@ const url = 'http://localhost:8000/reset-password'
 export async function resetPassService(data) {
   try {
     const response = await axios.post(url, data);
-    console.log(response);
     return response;
   }catch(err){
-    console.log(err);
     return err.response;
   };
 };
