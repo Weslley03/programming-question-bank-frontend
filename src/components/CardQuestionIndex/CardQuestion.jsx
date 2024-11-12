7066ded09243bdd9bc3a7d3ee4602c641b471f29
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-11 23:26:18 -0300

    created the `CardQuestion` component and your style

---
+++ src/components/CardQuestionIndex/CardQuestion.jsx
@@ -0,0 +1,22 @@
+import './CardQuestion.css';
+
+export function CardQuestion() {
+  return (
+    <>
+      <div id='casca'>
+        <div id='interior' className='d-flex justify-content-between align-items-center'>
+          <span> Author </span>
+          <span> Assunto </span>
+          <span> Texto da pergunta reduzido caso passe do lim.... </span>
+          <div className='d-flex flex-column text-end'>
+            <span> aprovada? </span>
+            <span> respondida 20 vezes </span>
+          </div>
+        </div>
+      </div>
+      <button className='prevButton'> before </button>
+      <span> { `>` } </span>
+      <button className='prevButton'> next </button>
+    </>
+  );
+};
